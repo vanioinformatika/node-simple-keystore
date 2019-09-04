1754cce8f7631edff5ca61895fb9414434f26ff8
Kovács Vince <vincent.kovacs@idomsoft.hu>
2019-09-04 15:32:24 +0200

    remove default factory function

--- lib/index.ts
+++ lib/index.ts
@@ -1,15 +1,4 @@
-import {KeystoreReaderFs} from './keystore.reader.fs'
-import {Keystore} from './keystore'
-
 export {CertificateNotFoundError, PrivateKeyNotFoundError, KeystoreError} from './errors'
 export {KeystoreReaderFs} from './keystore.reader.fs'
 export {KeyAndCert, JWK, Keystore, PrivateKey, Certificate} from './keystore'
 
-export default (debugNamePrefix: string,
-    baseDir: string,
-    refreshIntervalMillis: number,
-    signingKeyPassphrases: { [key: string]: string },
-): Keystore => {
-    const keystoreReader = new KeystoreReaderFs(debugNamePrefix, baseDir).readKeys
-    return new Keystore(debugNamePrefix, signingKeyPassphrases, keystoreReader, refreshIntervalMillis)
-}
