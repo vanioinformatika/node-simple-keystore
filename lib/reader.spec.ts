73a19e8ee71c157131eaeb9a69470b3eb0bfd747
Kovács Vince <vincent.kovacs@idomsoft.hu>
2019-09-16 11:41:55 +0200

    fix(test): setting base dir for ReaderFs

--- lib/reader.spec.ts
+++ lib/reader.spec.ts
@@ -8,13 +8,11 @@ import dirtyChai = require('dirty-chai')
 chai.use(chaiAsPromised)
 chai.use(dirtyChai)
 
-const debugName = 'keystore'
-
 describe('keystoreReaderFs', () => {
     const keys = new Map()
     it('Should read all RSA keys in a directory', async () => {
         const baseDir = path.join(__dirname, '/../test/keys_ca/rsa')
-        const keystoreReaderFs = new KeystoreReaderFs(debugName, baseDir)
+        const keystoreReaderFs = new KeystoreReaderFs(baseDir)
 
         const newKeys = await keystoreReaderFs.readKeys(keys)
         newKeys.forEach(key => {
@@ -37,7 +35,7 @@ describe('keystoreReaderFs', () => {
     })
     it('Should read all ECDSA keys in a directory', async () => {
         const baseDir = path.join(__dirname, '/../test/keys_ca/ecdsa')
-        const keystoreReaderFs = new KeystoreReaderFs(debugName, baseDir)
+        const keystoreReaderFs = new KeystoreReaderFs(baseDir)
         const newKeys = await keystoreReaderFs.readKeys(keys)
         newKeys.forEach(key => {
             expect(key).has.property('timestamp')
@@ -60,6 +58,6 @@ describe('keystoreReaderFs', () => {
     })
     it('Should fail if a wrong directory is passed', function () {
         const fakeBaseDir = path.join(__dirname, '/../test/keys_ca/_fake_')
-        expect(() => new KeystoreReaderFs('keystore', fakeBaseDir)).to.throw(Error, /ENOENT/)
+        expect(() => new KeystoreReaderFs(fakeBaseDir)).to.throw(Error, /ENOENT/)
     })
 })
